e9decb7ff9250e3286b83603680e91cc9288d4b6
KVC <yulinkeji@KVC.local>
2015-04-27 08:16:51 +0800

    Initial Commit

---
+++ iOSHotel/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOSHotel
+//
+//  Created by KVC on 15-4-27.
+//  Copyright (c) 2015年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOSHotel/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOSHotel
+//
+//  Created by KVC on 15-4-27.
+//  Copyright (c) 2015年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
